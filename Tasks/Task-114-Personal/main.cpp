fc392b173bcc7cccd32c7d981a2f9a2319e1929e
RobertGuy1 <robert.guy@students.plymouth.ac.uk>
2022-10-27 11:00:31 +0100

    Update main.cpp

--- Tasks/Task-114-Personal/main.cpp
+++ Tasks/Task-114-Personal/main.cpp
@@ -1,21 +1,37 @@
 #include "mbed.h"
 
-DigitalOut redLED(PC_2);
-DigitalOut yellowLED(PC_3);
-DigitalOut greenLED (PC_6);
+DigitalOut redLED(PC_2,1);
+DigitalOut yellowLED(PC_3,1);
+DigitalOut greenLED (PC_6,1);
 // main() runs in its own thread in the OS
 int main()
 {
-
-int count = 0;
-while (count < 4) {
+ wait_us(2000000);
+ int count = 0;  
+ redLED = 0;
+ yellowLED = 0;
+ greenLED = 0; 
+ while (count < 4)
+ {
     redLED = 1;
-    wait_us(250000);
+    wait_us(2000000);
+    yellowLED = 1;
+    wait_us(2000000);
     redLED = 0;
-    wait_us(250000);
-    count = count + 1;
-
+    yellowLED = 0;
+    greenLED = 1;
+    wait_us(2000000);
+    greenLED = 0;
     
-}
+    for (int pog = 0; pog < 4; pog++) 
+    {
+    yellowLED = 1;
+    wait_us(250000);
+    yellowLED = 0;
+    wait_us(250000);
+    }
+
+    count = count + 1;
+ }
 
 }
